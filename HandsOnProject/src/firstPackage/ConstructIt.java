6c9d07e4ed027290bc4a5ac21364164aba2ca9b3
rahul <rahul@DESKTOP-UC82OPD>
2018-06-23 17:26:10 +0530

    Second commit

--- HandsOnProject/src/firstPackage/ConstructIt.java
+++ HandsOnProject/src/firstPackage/ConstructIt.java
@@ -7,9 +7,13 @@ public class ConstructIt {
 	public float f = 2.5f;
 	double d = 55.55;
 	
+	public ConstructIt(int i) {
+		// TODO Auto-generated constructor stub
+		this.i = i;
+		
+	}
+	
 	public ConstructIt() {
 		// TODO Auto-generated constructor stub
-		this.i = 4;
-		
 	}
 }
